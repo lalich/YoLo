b6c04bc605308aa44c183427f144a9336fd7dfa1
Mark D. Lalich <mdlalich@gmail.com>
2023-07-05 18:05:23 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// Import our dependencies
+require('dotenv').config(); // bring in our .env vars
+const express = require('express'); // web framework for node
+const morgan = require('morgan'); // logger for node
+const methodOverride = require('method-override'); // allows us to use PUT and DELETE methods
+// express application
+const app = express();
+
+// middleware
+app.use(morgan('tiny')); // logging
+app.use(methodOverride('_method')); // override with POST having ?_method=DELETE or ?_method=PUT
+app.use(express.static('public')); // serve static files from public folder
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.send('Yo git Fukd!');
+    })
+
+    const PORT = process.env.PORT
+    app.listen(PORT, () => { console.log(`Listening on port ${PORT}`)})
