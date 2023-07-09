af9b47912f7a978006367e22c79a0cbd86f12b39
Mark D. Lalich <mdlalich@gmail.com>
2023-07-09 07:38:24 -0700

    les get the user experience going

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('./connections')
+
+const { Schema, model} = mongoose
+
+const userSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    password: { type: String, requireed: true, },
+})
+
+const User = model('user', userSchema)
+
+model.exports = User
