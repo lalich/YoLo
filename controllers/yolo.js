cc12767f141706ef406aea456e3611270704fb1f
Mark D. Lalich <mdlalich@gmail.com>
2023-07-12 19:25:22 -0700

    a new type of roulette wheel

--- controllers/yolo.js
+++ controllers/yolo.js
@@ -29,9 +29,14 @@ router.get('/addie', (req, res) => {
     res.render('createYolo.ejs')
 })
 
+router.get('/wsry', (req, res) => {
+    res.render('wsry.ejs')
+})
+
 
 router.post('/', async (req, res) => {
     req.body.yolo = req.body.yolo === 'on' ? true : false
+    req.body.riskP = req.body.yolo ? 100 : req.body.riskP   
     req.body.username = req.session.username
 
         await Yolos.create(req.body)
@@ -40,7 +45,31 @@ router.post('/', async (req, res) => {
     res.redirect('/yolos')
 })
 
+router.post('/wsryC', async (req, res) => {
+//     const tickerSet = ['AAPL', 'GOOGL', 'AMZN', 'META', 'INTC', 'SOFI', 'A', 'GME', 'BB', 'CVX',
+//                         'XOM', 'T', 'NIO', 'BABA', 'MSFT', 'TSLA', 'NVDA', 'HD', 'PG', 'KO',
+//                         'COST', 'CRM', 'MCD', 'NFLX', 'F', 'AMD', 'TMUS', 'NKE', 'DIS', 'RTX',
+//                         'BA', 'CAT', 'UNP', 'TM', 'PEP', 'SPY', 'QQQ', 'PLTR']
+// console.log(tickerSet[5])
+//         function randomT(tickerSet) { 
+//                     const randomIndex= Math.floor(Math.random() * tickerSet.length)
+//                         return tickerSet[randomIndex]
+//                         }
+//                         console.log(randomT())
 
+    req.body.ticker = randomT(tickerSet)
+    req.body.amount = req.body.amount
+    req.body.yolo = req.body.yolo === 'on'
+    req.body.riskP = 100
+    req.body.profitP = req.body.profitP
+    req.body.duration = 30
+    req.body.username = req.session.username
+
+        await Yolos.create(req.body)
+
+  
+    res.redirect('/yolos')
+})
 
 
 
@@ -57,7 +86,8 @@ res.render('edit.ejs', { yolo })
 
 
 router.put('/:id', async (req, res) => {
-req.body.yolo = req.body.yolo === 'on' ? true : false
+    req.body.yolo = req.body.yolo === 'on' ? true : false
+    req.body.riskP = req.body.yolo ? 100 : req.body.riskP  
 await Yolos.findByIdAndUpdate(req.params.id, req.body)
 res.redirect('/yolos')
 })
@@ -68,7 +98,6 @@ await Yolos.findByIdAndDelete(req.params.id)
 const deletedYolo = await Yolos.findByIdAndDelete(req.params.id)
 res.redirect('/yolos')
 })
-
 
 
 module.exports = router
